9405193ac83d1f2420938ce4ee429a088d3f4946
Ricardo Moura <rikardomoura1@gmail.com>
2020-11-07 22:59:06 -0300

    Trying to fix select level bug

---
+++ Assets/Scripts/LevelSelectPlayer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectPlayer : MonoBehaviour
+{
+
+    public MapPoint currentPoint;
+    public float moveSpeed = 10f;
+
+    public bool isMoving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        transform.position = Vector3.MoveTowards(transform.position,currentPoint.transform.position,moveSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position,currentPoint.transform.position) < .1f){
+            
+            Debug.Log(transform.position +"|" +currentPoint.transform.position);
+
+            if(Input.GetAxisRaw("Horizontal") > .5f){
+                if(currentPoint.right != null){
+                    setNextPoint(currentPoint.right);
+                }
+            }
+
+
+            if(Input.GetAxisRaw("Horizontal") < -.5f){
+                if(currentPoint.left != null){
+                    setNextPoint(currentPoint.left);
+                }   
+            }
+
+            if(Input.GetAxisRaw("Vertical") > .5f){
+                if(currentPoint.up != null){
+                    setNextPoint(currentPoint.up);
+                }   
+            }
+
+            if(Input.GetAxisRaw("Vertical") < -.5f){
+                if(currentPoint.down != null){
+                    setNextPoint(currentPoint.down);
+                }   
+            }
+
+        }
+
+
+
+    }
+
+
+    public void setNextPoint(MapPoint nextPoint){
+        currentPoint = nextPoint;
+    }
+
+}
